d5074bdfbd7a1eebc8bbaf6c83604e24b7520b51
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2024-03-11 14:32:33 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'CosminREMAX',
+    themeColor: '#000000',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/cosmin-draghici-remax/'
     : '/'
