0a69ebb4503771d7a227fb517c3a88546c615349
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2024-03-11 14:43:11 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,12 @@ module.exports = defineConfig({
     appleMobileWebAppStatusBarStyle: 'black',
 
     // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/cosmin-draghici-remax/'
